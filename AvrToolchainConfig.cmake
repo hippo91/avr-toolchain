299e0c06b577377f84e3d98d698dff87422835dc
Guillaume Peillex <guillaume.peillex@gmail.com>
2026-03-21 16:02:46 +0100

    Initial import

---
+++ AvrToolchainConfig.cmake
@@ -0,0 +1,10 @@
+get_filename_component(PACKAGE_PREFIX_DIR "${CMAKE_CURRENT_LIST_DIR}/" ABSOLUTE)
+
+set(AvrToolchain_LOCATION ${PACKAGE_PREFIX_DIR}/avr-toolchain.cmake)
+if(EXISTS ${AvrToolchain_LOCATION})
+  set(AvrToolchain_FOUND True)
+else()
+  message(AvrToolchain not found!)
+  set(AvrToolchain_FOUND False)
+endif()
+
